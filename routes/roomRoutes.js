e6cdd8306413f984f4ec121265ae84377cbfcdb7
ChathurikaThathsarani <it20603168@my.sliit.lk>
2023-05-07 09:48:40 +0530

    Create customer routes for reservations

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const { getRooms, createRoom, getRoomById, updateRoom, deleteRoom } = require("../controllers/roomController");
 const { getRooms } = require("../controllers/roomController");
+const {
+	getReservations,
+	addReservation,
+	updateReservation,
+	deleteReservation,
+	getTotal,
+} = require("../controllers/reservationController");
 const { protectAdmin } = require("../middleware/authAdminMiddleware");
 const router = express.Router();
 
@@ -10,5 +17,11 @@ router.route("/room/:id").get(protectAdmin, getRoomById).put(protectAdmin, updat
 router.route("/room/delete/:id").delete(protectAdmin, deleteRoom);
 
 router.route("/rooms/:id").get(getRooms);
+
+router.route("/reservation/create").post(addReservation);
+router.route("/reservation/get/:id").get(getReservations);
+router.route("/reservation/get/total/:id").get(getTotal);
+router.route("/reservation/update/:id").put(updateReservation);
+router.route("/reservation/delete/:id").delete(deleteReservation);
 
 module.exports = router;
