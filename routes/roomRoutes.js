3e9aeab85a7f998df4fbf5e333e2f360728cd530
ChathurikaThathsarani <it20603168@my.sliit.lk>
2023-05-07 09:47:35 +0530

    Create customer routes for rooms

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { getRooms, createRoom, getRoomById, updateRoom, deleteRoom } = require("../controllers/roomController");
+const { getRooms } = require("../controllers/roomController");
 const { protectAdmin } = require("../middleware/authAdminMiddleware");
 const router = express.Router();
 
@@ -7,5 +8,7 @@ router.route("/room/create").post(protectAdmin, createRoom);
 router.route("/get-rooms/:id").get(protectAdmin, getRooms);
 router.route("/room/:id").get(protectAdmin, getRoomById).put(protectAdmin, updateRoom);
 router.route("/room/delete/:id").delete(protectAdmin, deleteRoom);
+
+router.route("/rooms/:id").get(getRooms);
 
 module.exports = router;
